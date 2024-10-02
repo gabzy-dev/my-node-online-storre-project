df2857580d8d2c720e4f0806c163005fa7b7c7a8
gabzy-dev <george.egabor31@gmail.com>
2024-10-02 10:19:07 -0500

    changes made

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use(express.static(path.join(__dirname,'Public')))
 app.use('/admin',adminData.routes);
 app.use(shopRoute);
 
+app.set('view engine','pug');
+app.set('Views', 'Views');
+
 app.use((req,res,next)=> {
   res.status(404).sendFile((path.join(__dirname,"/","Views","Error.html" )));
 });
