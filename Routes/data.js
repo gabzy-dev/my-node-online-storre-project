794900c9b28f92cf229e965f1550f0178c74cad0
gabzy-dev <george.egabor31@gmail.com>
2024-10-25 09:58:04 -0500

    using pug systems

--- Routes/data.js
+++ Routes/data.js
@@ -6,7 +6,7 @@ const products = [];
 
 router.get("/add-product",(req,res,next)=> {//allows request to move into the next middleware.
     //next should be called if we dont send a response.
-    res.render('add-product', {pageTitle: "Add-product page"} )
+    res.render('add-product', {pageTitle: "Add-product page",path:"/admin/add-product"})
   });
 
 
--- Routes/shop.js
+++ Routes/shop.js
@@ -7,7 +7,7 @@ const adminData = require("./data");
 
 router.get('/',(req,res,next)=> {
   const products = adminData.products;
-    res.render('Shop',{prods:products, docTitle: 'Shop'});
+    res.render('Shop',{prods:products, docTitle: 'Shop',path: "/"});
   })
 
 
