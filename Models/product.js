a2bf58a6e2372b9906c836cbf15a139e1601b4fd
gabzy-dev <george.egabor31@gmail.com>
2024-11-08 06:50:14 -0600

    updated!

--- Models/product.js
+++ Models/product.js
@@ -1,23 +1,60 @@
-const products = [];
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(require.main.filename),'data','products.json');
+
+const getProductFromFile =cb=>{
+      fs.readFile(p,(err,fileContent)=>{
+        if(err){
+          return  cb([])
+        }else{
+            cb(JSON.parse(fileContent))
+        }
+      }) 
+
+}
+
 module.exports = class Product {
     constructor(t){
         this.title = t;
-
     }
     
     save(){
-        const p = path.join(__dirname,'data','products.json');
-        fs.readFile(p,(err,fileContent)=>{
-            console.log(err)
-        })
+        getProductFromFile((products=>{
+          products.push(this) ;
+            fs.writeFile(p, JSON.stringify(products,null,2), (err)=>{
+             if(err){
+                console.error('error writing to products.json',err)
+             }else{
+                console.log('product saved successfully',this)
+             }
+            });
+        }))
+        // const p = path.join(path.dirname(require.main.filename),'data','products.json');
+        // fs.readFile(p, (err, fileContent) => { 
+        //     let products = [];
+        //     if(!err){  
+        //         try{
+        //             products = JSON.parse(fileContent);
+        //         } catch(parseError){
+        //             console.error('Error parsing JSON, starting with an empty array:', parseError);
+        //         }
+          
+        //     }  })
+            // products.push(this) ;
+            // fs.writeFile(p, JSON.stringify(products,null,2), (err)=>{
+            //  if(err){
+            //     console.error('error writing to products.json',err)
+            //  }else{
+            //     console.log('product saved successfully',this)
+            //  }
+            // });
+      
         // products.push(this)
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+     getProductFromFile(cb)
+
     }
-
-
-}
+};
